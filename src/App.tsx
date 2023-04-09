97e47b110439e4edb7ab9b784be86ea13962aacc
MaryAnzh <maryang@mail.ru>
2023-04-09 21:05:40 +0300

    feat: add reset style

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Route, Router, Routes, BrowserRouter } from 'react-router-dom';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import { Header } from './components/common/header';
 import { Footer } from './components/common/footer';
 import { MainStyle } from './AppStyled';
-import { GamePage } from './components/pages/game';
+import { GamePage } from './pages/game';
 
 import { APPStyled } from './AppStyled';
 
