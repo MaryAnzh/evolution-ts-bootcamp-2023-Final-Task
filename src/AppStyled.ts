81f03dc85064a5865ca339d72a03652d3c25353a
MaryAnzh <maryang@mail.ru>
2023-04-10 10:49:57 +0300

    feat: add base style

--- src/AppStyled.ts
+++ src/AppStyled.ts
@@ -1,9 +1,17 @@
 import styled from "styled-components";
 
 export const APPStyled = styled.div`
-  background: pink;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
 export const MainStyle = styled.main`
   width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  flex: 1 0 auto;
 `;
--- src/components/common/footer/styled.ts
+++ src/components/common/footer/styled.ts
@@ -1,4 +1,6 @@
 import styled from "styled-components";
 
 export const FooterStyle = styled.footer`
-  width: 100%`;
+  width: 100%;
+  flex-shrink: 0;
+`;
