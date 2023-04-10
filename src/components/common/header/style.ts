c987a05dc71e5c336d174752d669a542d5f1ca06
MaryAnzh <maryang@mail.ru>
2023-04-10 10:56:47 +0300

    feat: add base style

--- src/components/common/header/style.ts
+++ src/components/common/header/style.ts
@@ -1,4 +1,9 @@
 import styled from "styled-components";
 
 export const HeaderStyle = styled.header`
-  width: 100%`;
+  width: 100%;
+  padding: 1rem;
+  display: flex;
+  align-items: center;
+  background: pink;
+  `;
--- src/pages/game/style.ts
+++ src/pages/game/style.ts
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const GamePageStyle = styled.div`
   width: 100%;
+  padding: 0 1rem;
   background: blue;
   color: white;
 `;
